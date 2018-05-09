26dba81870e77116bc237a3f5714c885feb04ae5
Edwin Hsia <edwinhsia@gmail.com>
2018-05-09 10:33:00 -0500

    Setting up pages

---
+++ plant-tracker/src/pages/IndoorPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const IndoorPage = props => {
+
+    // Share buttons
+    <div className="sharethis-inline-share-buttons"></div>
+}
+
+export default IndoorPage;
---
+++ plant-tracker/src/pages/OutdoorPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OutdoorPage = props => {
+
+    // Share buttons
+    <div className="sharethis-inline-share-buttons"></div>
+}
+
+export default OutdoorPage;
