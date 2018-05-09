5a9a76d10aed8ac0bbfa0d4a5c7d6aabe3f02d0e
mguldberg <guldberg@sbcglobal.net>
2018-05-09 11:16:38 -0500

    added DB sequelize models

---
+++ models/userPlants.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, DataTypes) {
+    const UserPlant = sequelize.define("UserPlant", {
+        plantName: {
+            type: DataTypes.STRING,
+            // len is a validation that checks that our todo is between 1 and 140 characters
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [3, 30],
+                    msg: "Your plant name is not long enough or too long.  It must be between 3 and 30 characters."
+                }
+            }
+        },
+        needsToBeWatered_bool: {
+            type: DataTypes.BOOLEAN,
+            // defaultValue to false for needing to be watered
+            defaultValue: false
+        },
+        // frequency in days of needing to be watered
+        wateringInterval:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+        },
+        // last watered date and time
+        lastWateredDate:{
+            type:DataTypes.DATEONLY,
+            defaultValue: sequelize.NOW
+        },
+        //alive, dead, needs attention
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'alive'
+        },
+        //valid types are indoor, outdoor
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+
+    });
+
+    UserPlant.associate = function (models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        UserPlant.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            },
+        });
+    };
+
+    return UserPlant;
+};
+
