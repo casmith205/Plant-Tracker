31120dbbe116b9490e9c82aedbab9311c9172023
BrittanyG <brin.greaner@gmail.com>
2018-05-09 11:07:35 -0500

    react route structure

--- plant-tracker/src/App.js
+++ plant-tracker/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import LoginPage from './pages/LoginPage';
+// import RegistrationPage from './pages/RegistrationPage';
+// import ProfilePage from './pages/ProfilePage';
+// import IndoorPage from './pages/IndoorPage';
+// import OutdoorPage from './pages/OutdoorPage';
+// import GraveyardPage from'./pages/GraveyardPage';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // don't need anything outside of the routes
+      <Router>
+        <Switch>
+          {/* <Route exact path="/" component={LoginPage} /> */}
+          {/* <Route exact path="/registration" component={RegistrationPage} /> */}
+          {/* <Route exact path="/profile" component={ProfilePage} /> */}
+          {/* <Route exact path="/outdoorplants" component={GraveyardPage} />
+          <Route exact path="/indoorplants" component={IndoorPage} /> */}
+          {/* <Route exact path="/profile" component={ProfilePage} /> */}
+          {/* <Route exact path="/registration" component={RegistrationPage} /> */}
+          {/* determines what component is routed to.
+          below route (NoMatch) would be 404*/}
+          {/* <Route component={NoMatch} />  */}
+        </Switch> 
+      </Router>
+
     );
-  }
+  };
 }
 
 export default App;
