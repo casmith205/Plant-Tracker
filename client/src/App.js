e59c942dcaf44b863776e0d233e39b8b2b42c248
Carolyn Smith <casmith205@gmail.com>
2018-05-09 11:18:24 -0500

    updating master

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
-import LoginPage from './pages/LoginPage';
+// import LoginPage from './pages/LoginPage';
+// import RegistrationPage from './pages/RegistrationPage';
+// import ProfilePage from './pages/ProfilePage';
+// import IndoorPage from './pages/IndoorPage';
+// import OutdoorPage from './pages/OutdoorPage';
+// import GraveyardPage from'./pages/GraveyardPage';
+
 import './App.css';
 
-const App = () =>
-  <Router>
-    <div>
-      <Nav />
-      <Route path="/" component={LoginPage} />
-    </div>
-  </Router>;
+class App extends Component {
+  render() {
+    return (
+      // don't need anything outside of the routes
+      <Router>
+        <Switch>
+          {/* <Route exact path="/" component={LoginPage} /> */}
+          {/* <Route exact path="/registration" component={RegistrationPage} /> */}
+          {/* <Route exact path="/profile" component={ProfilePage} /> */}
+          {/* <Route exact path="/outdoorplants" component={GraveyardPage} />
+          <Route exact path="/indoorplants" component={IndoorPage} /> */}
+          {/* <Route exact path="/profile" component={ProfilePage} /> */}
+          {/* <Route exact path="/registration" component={RegistrationPage} /> */}
+          {/* determines what component is routed to.
+          below route (NoMatch) would be 404*/}
+          {/* <Route component={NoMatch} />  */}
+        </Switch> 
+      </Router>
+
+    );
+  };
+}
 
 export default App;
