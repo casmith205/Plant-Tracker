610a10bddfef97448ecc270edc2e1eb3f224b631
BrittanyG <brin.greaner@gmail.com>
2018-05-10 12:03:10 -0500

    fixed some login

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
-import { Input } from "../../components/Login"
-import "./LoginPage.css";
+import Input from '../../components/Login/Input'
+import styles from "./LoginPage.css";
 
 class LoginPage extends Component {
     state = {
@@ -61,56 +61,58 @@ class LoginPage extends Component {
         // };
 
         render() {
-    <div className="container" id="loginpage">
-        <div className="row">
-            <form className="col s12">
-                <div className="row">
-                    <Input
-                        name="userlogin"
-                        value={this.state.username}
-                        onChange={this.handleInputChange}
-                        placeholder="Username"
-                    />
+            return (
+                <div className="container" id="loginpage">
+                    <div className="row">
+                        <form className="col s12">
+                            <div className="row">
+                                <Input
+                                    name="userlogin"
+                                    value={this.state.username}
+                                    onChange={this.handleInputChange}
+                                    placeholder="Username"
+                                />
+                            </div>
+                            <div class="row">
+                                <Input
+                                    name="userlogin"
+                                    value={this.state.password}
+                                    onChange={this.handleInputChange}
+                                placeholder="Password"
+                                />
+                            </div>
+                            <div className="col s6">
+                                <a
+                                    onClick={this.handleFormSubmit}
+                                    type="success"
+                                    className="input-lg waves-effect waves-light btn"
+                                >
+                                    Login
+                                </a>
+                            </div>
+                            <div className="col s6">
+                                <a
+                                    onClick={this.handleFormSubmit}
+                                    type="success"
+                                    className="input-lg waves-effect waves-light btn"
+                                >
+                                    Signup
+                                </a>
+                            </div>
+                            {/* <div className="col s6">
+                                <a
+                                    onClick={this.handleHelpSubmit}
+                                    type="success"
+                                    className="input-lg waves-effect waves-light btn"
+                                >
+                                    Help Logging In
+                                </a>
+                            </div> */}
+                    </form>
                 </div>
-                <div class="row">
-                    <Input
-                        name="userlogin"
-                        value={this.state.password}
-                        onChange={this.handleInputChange}
-                        placeholder="Password"
-                    />
-                </div>
-                <div className="col s6">
-                    <a
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg waves-effect waves-light btn"
-                    >
-                        Login
-                      </a>
-                </div>
-                <div className="col s6">
-                    <a
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg waves-effect waves-light btn"
-                    >
-                        Signup
-                      </a>
-                </div>
-                {/* <div className="col s6">
-                        <a
-                            onClick={this.handleHelpSubmit}
-                            type="success"
-                            className="input-lg waves-effect waves-light btn"
-                        >
-                            Help Logging In
-                      </a>
-                    </div> */}
-            </form>
-        </div>
-    </div>
-}
-}
+            </div>
+            )
+        }
+    }
 
 export default LoginPage;
