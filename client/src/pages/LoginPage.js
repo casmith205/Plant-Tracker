670c04d34210bc486a360f15c957af09d6a2c332
Edwin Hsia <edwinhsia@gmail.com>
2018-05-10 10:46:46 -0500

    Adding Components

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
 import "./LoginPage.css";
 
-const LoginPage = props => {
-    <div className="container" id="loginpage">
-        <div class="row">
-            <form class="col s12">
+class LoginPage extends Component {
+    state = {
+        user: "",
+        password: ""
+    };
+
+    handleLogin = event => {
+        const {name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    };
+
+    render() {
+        <div className="container" id="loginpage">
             <div class="row">
-                    <div class="input-field col s12">
-                        <input id="userlogin" type="text" class="validate" placeholder="Username"></input>
-                        <label for="userlogin">Username</label>
+                <form class="col s12">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="userlogin" type="text" class="validate" placeholder="Username"></input>
+                            <label for="userlogin">Username</label>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="userpassword" type="password" class="validate" placeholder="Password"></input>
-                        <label for="userpassword">Password</label>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="userpassword" type="password" class="validate" placeholder="Password"></input>
+                            <label for="userpassword">Password</label>
+                        </div>
                     </div>
-                </div>
-                <a class="waves-effect waves-light btn">Login</a>
-                <a class="waves-effect waves-light btn">Help Logging In</a>
-            </form>
+                    <a class="waves-effect waves-light btn">Login</a>
+                    <a class="waves-effect waves-light btn">Help Logging In</a>
+                </form>
+            </div>
         </div>
-    </div>
+    }
 }
 
 export default LoginPage;
