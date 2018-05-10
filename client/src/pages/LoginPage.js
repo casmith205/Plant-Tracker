e5b2c0845d82be7f801b5a061a048d797c482748
Edwin Hsia <edwinhsia@gmail.com>
2018-05-09 20:01:41 -0500

    Updated background and styling

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,19 +1,20 @@
 import React from "react";
+import "./LoginPage.css";
 
 const LoginPage = props => {
-    <div className="container">
+    <div className="container" id="loginpage">
         <div class="row">
             <form class="col s12">
             <div class="row">
                     <div class="input-field col s12">
                         <input id="userlogin" type="text" class="validate" placeholder="Username"></input>
-                        <label for="email">Username</label>
+                        <label for="userlogin">Username</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12">
                         <input id="userpassword" type="password" class="validate" placeholder="Password"></input>
-                        <label for="password">Password</label>
+                        <label for="userpassword">Password</label>
                     </div>
                 </div>
                 <a class="waves-effect waves-light btn">Login</a>
