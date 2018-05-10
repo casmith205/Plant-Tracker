f6c3e44f03c24e73a26b9421787b35e5451ebfbf
Carolyn Smith <casmith205@gmail.com>
2018-05-10 11:16:08 -0500

    editing user model

--- models/users.js
+++ models/users.js
@@ -51,11 +51,11 @@ module.exports = function (sequelize, DataTypes) {
         }
       }
     },
-    address:{
+    address: {
       type: DataTypes.STRING,
       allowNull: true
     },
-    zipCode:{
+    zipCode: {
       type: DataTypes.INTEGER,
       allowNull: true,
       validate: {
@@ -79,18 +79,18 @@ module.exports = function (sequelize, DataTypes) {
   };
   console.log("User End")
 
-  return User;
-};
-
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.hook("beforeCreate", function(user) {
+  User.hook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
   return User;
 };
+
+
+
 
