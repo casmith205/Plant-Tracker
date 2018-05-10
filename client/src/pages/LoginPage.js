e2640a0e0775bf096f54d15ac208c3552947ee5b
Edwin Hsia <edwinhsia@gmail.com>
2018-05-10 11:31:12 -0500

    updated Login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Login"
 import "./LoginPage.css";
 
 class LoginPage extends Component {
@@ -28,49 +29,76 @@ class LoginPage extends Component {
         }
     };
 
-    // Will need to add some helper on this but I don't know how to do this.
-    // handleHelpSubmit = event => {
-    //     event.preventDefault();
-    //     if (this.state.username && this.state.userpassword) {
-    //         API.loginUser({
-    //             username: this.state.username,
-    //             password: this.state.password
-    //         })
-    //             .then(res => this.handleUserLogin())
-    //             .catch(err => console.log(err));
-    //     }
-    // };
+    handleLoginSubmit = event => {
+        event.preventDefault();
+        if (this.state.username && this.state.userpassword) {
+            API.registerUser({
+                username: this.state.username,
+                password: this.state.password,
+                firstname: this.state.firstname,
+                lastname: this.state.lastname
+            })
+                .then(res => this.handleUserLogin())
+                .catch(err => console.log(err));
+        }
+    };
 
-    render() {
-        <div className="container" id="loginpage">
-            <div className="row">
-                <form className="col s12">
-                    <div className="row">
-                        <Input
-                            name="userlogin"
-                            value={this.state.username}
-                            onChange={this.handleInputChange}
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div class="row">
-                        <Input
-                            name="userlogin"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="col s6">
-                        <a
-                            onClick={this.handleFormSubmit}
-                            type="success"
-                            className="input-lg waves-effect waves-light btn"
-                        >
-                            Login
+    // handleUserLogin = event => {
+
+    // }
+
+        // Will need to add some helper on this but I don't know how to do this.
+        // handleHelpSubmit = event => {
+        //     event.preventDefault();
+        //     if (this.state.username && this.state.userpassword) {
+        //         API.loginUser({
+        //             username: this.state.username,
+        //             password: this.state.password
+        //         })
+        //             .then(res => this.handleUserLogin())
+        //             .catch(err => console.log(err));
+        //     }
+        // };
+
+        render() {
+    <div className="container" id="loginpage">
+        <div className="row">
+            <form className="col s12">
+                <div className="row">
+                    <Input
+                        name="userlogin"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        placeholder="Username"
+                    />
+                </div>
+                <div class="row">
+                    <Input
+                        name="userlogin"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="col s6">
+                    <a
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg waves-effect waves-light btn"
+                    >
+                        Login
                       </a>
-                    </div>
-                    {/* <div className="col s6">
+                </div>
+                <div className="col s6">
+                    <a
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg waves-effect waves-light btn"
+                    >
+                        Signup
+                      </a>
+                </div>
+                {/* <div className="col s6">
                         <a
                             onClick={this.handleHelpSubmit}
                             type="success"
@@ -79,10 +107,10 @@ class LoginPage extends Component {
                             Help Logging In
                       </a>
                     </div> */}
-                </form>
-            </div>
+            </form>
         </div>
-    }
+    </div>
+}
 }
 
 export default LoginPage;
