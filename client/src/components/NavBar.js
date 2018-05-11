3f262125b8a81cbdad262b0ee7ee1c21fbf88f27
BrittanyG <brin.greaner@gmail.com>
2018-05-10 22:04:28 -0500

    fixed some navbar things

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
 
-const NavBar = props => {
+class NavBar extends Component {
+render () {
+    return (
 
-}
+    <div>
+        <nav brand='logo' right>
+            <NavItem href='Outdoor pg'><i>local_florist</i></NavItem>
+            <NavItem href='Indoor pg'><i>home</i></NavItem>
+            <NavItem href='Graveyard pg'><i>broken_image</i></NavItem>
+            <NavItem href='Profile pg'><i>person</i></NavItem>
+        </nav>
+    </div>
+    )};
+};
 
 export default NavBar;
