b356adc6b6f275de479f61fb47173f04d51d9d65
Edwin Hsia <edwinhsia@gmail.com>
2018-05-13 09:27:20 -0500

    Adding background

--- client/src/pages/GraveyardPage/GraveyardPage.js
+++ client/src/pages/GraveyardPage/GraveyardPage.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { DeadPlantInfo, Tombstone } from "../../components/Inside/";
+import API from "../../utils/API";
 
-const GraveyardPage = props => (
+class GraveyardPage extends Component {
 
-    <div>
-        <h1>Welcome to the spooooooooky graveyard</h1>
-    </div>
-    //     {/* //call gravestone on click
-    //     //call hover modal on hover */}
-    // </div>
-)
+
+    render() {
+        return (
+            <div id="graveyardpage">
+                <h1>Welcome to the spooooooooky graveyard</h1>
+                <Tombstone />
+            </div>
+
+            //     {/* //call gravestone on click
+            //     //call hover modal on hover */}
+            // </div>
+        )
+    }
+}
 
 export default GraveyardPage;
