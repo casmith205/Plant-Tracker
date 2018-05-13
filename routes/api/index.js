ae1dcab6cf9f39f9f2ef573f0c2de0dbbdbff686
Carolyn Smith <casmith205@gmail.com>
2018-05-13 17:02:56 -0500

    minor comments in files

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 // REQUIRE API ROUTES
 const plantRoutes = require("./plants");
 
-// Book routes
+// Plant routes
 router.use("/plants", plantRoutes);
 
 module.exports = router;
