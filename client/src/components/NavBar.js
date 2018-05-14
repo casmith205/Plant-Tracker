009a2cf3567e4e3bcd8c4158ec074f3983d84c75
Edwin Hsia <edwinhsia@gmail.com>
2018-05-14 12:53:35 -0500

    Added labels

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,10 +8,10 @@ class NavBar extends Component {
                 <nav brand='logo' right>
                     <div className="nav-wrapper">
                         <ul class="side-nav right" id="mobile-demo">
-                            <li><a href='/indoorplants'><i className="small material-icons">home</i></a></li>
-                            <li><a href='/outdoorplants'><i className="small material-icons">local_florist</i></a></li>
-                            <li><a href='/graveyard'><i className="small material-icons">broken_image</i></a></li>
-                            <li><a href='/profile'><i className="small material-icons">person</i></a></li>
+                            <li><a href='/indoorplants'><i className="small material-icons">home</i>Indoor</a></li>
+                            <li><a href='/outdoorplants'><i className="small material-icons">local_florist</i>Outdoor</a></li>
+                            <li><a href='/graveyard'><i className="small material-icons">broken_image</i>Graveyard</a></li>
+                            <li><a href='/profile'><i className="small material-icons">person</i>Profile</a></li>
                         </ul>
                         </div>
                         </nav>
