41a9972c0dd49c8853785b0be047aa81809c6bbc
Edwin Hsia <edwinhsia@gmail.com>
2018-05-14 12:50:47 -0500

    fixed responsive navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,8 +7,7 @@ class NavBar extends Component {
             <div>
                 <nav brand='logo' right>
                     <div className="nav-wrapper">
-
-                        <ul class="side-nav" id="mobile-demo">
+                        <ul class="side-nav right" id="mobile-demo">
                             <li><a href='/indoorplants'><i className="small material-icons">home</i></a></li>
                             <li><a href='/outdoorplants'><i className="small material-icons">local_florist</i></a></li>
                             <li><a href='/graveyard'><i className="small material-icons">broken_image</i></a></li>
