42709843382d26beed5d04e366744426d785d944
Cecilia Clohisy <cclohisy@gmail.com>
2018-05-14 11:30:30 -0500

    outside plant dropdown

--- client/src/components/Outside/Plants.js
+++ client/src/components/Outside/Plants.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { PlantInfo } from "../../components"
+import Draggable, { DraggableCore } from 'react-draggable'; //draggable
+import { PottedPlants } from "../Inside";
+
 
 export const Plants = props => (
-    <div>
-        <h4> This is the plants.js file. The word plants doesn't look real anymore </h4>
-        {/* <!-- Dropdown Trigger --> */}
-        <a className='dropdown-trigger btn' href='#' data-target="plantDropdown">Plant</a>
-        <PlantInfo />
-    </div>
+    <Draggable>
+
+        <div>
+            {/* <!-- Dropdown Trigger --> */}
+            <a className='dropdown-button btn' data-activates="plantDropdown">Outside Plant</a>
+            <PlantInfo
+                plantName="the coolest flower"
+                type="indoor"
+                status="alive"
+                needsWater="true" />
+        </div>
+    </Draggable>
 
 )
+
+// class OutsidePlants extends Component {
+//     render() {
+//         return (
+//             <Draggable>
+//             <div>
+//             <a className='dropdown-button btn' data-activates="plantDropdown">Potted Plant</a>
+//                     <PlantInfo 
+//                     plantName= "the coolest flower"
+//                     type = "indoor"
+//                     status = "alive"
+//                     needsWater = "true" /> 
+//                 </div>
+//             </Draggable>
+//         )
+//     }
+// }
+
+export default PottedPlants
