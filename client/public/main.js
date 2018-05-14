cd4d200a479dc6ebcefb30228aee7ed62966f434
Cecilia Clohisy <cclohisy@gmail.com>
2018-05-14 15:32:38 -0500

    modal content

--- client/public/main.js
+++ client/public/main.js
@@ -9,4 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+
 //   M.AutoInit();
--- client/src/components/AddPlantIcon.js
+++ client/src/components/AddPlantIcon.js
@@ -10,17 +10,22 @@ export const AddPlantIcon = props => (
         </div>
         <div id="addPlantModal" className="modal">
             <div className="modal-content">
-                <h4>Modal Header</h4>
+                <h4>Add a new plant to your family</h4>
                 <p>A bunch of text</p>
             </div>
+            <div class="input-field col s12">
+                <select>
+                    <option value="" disabled selected>Choose your option</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                </select>
+                <label>Materialize Select</label>
+            </div>
             <div className="modal-footer">
-                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Search</a>
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">ADD!</a>
             </div>
         </div>
-
-        {/* <h1>{props.name}</h1> */}
-
-
 
     </div>
 )
