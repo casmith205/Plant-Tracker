6d1734c15954759dc7017f9499d66e4840d7b89a
BrittanyG <brin.greaner@gmail.com>
2018-05-14 13:06:06 -0500

    added modal, nonfunctional

--- client/src/components/AddPlantIcon.js
+++ client/src/components/AddPlantIcon.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+// openSearch = () => {
+
+// }
+
 export const AddPlantIcon = props => (
     <div>
-        <h1>{props.name}</h1>
+        <a class="waves-effect waves-light btn modal-trigger" href="#addplanticon"><i class="large material-icons">add_circle_outline</i></a>
+        {/* <!-- Modal Structure --> */}
+            <div id="addplanticon" class="modal">
+                <div class="modal-content">
+                    <h4>Modal Header</h4>
+                    <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+            </div>
+        
+        {/* <h1>{props.name}</h1> */}
     </div>
 )
