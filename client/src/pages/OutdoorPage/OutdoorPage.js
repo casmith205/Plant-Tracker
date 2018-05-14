014c77d1b4eaff2d44c0590c53cc1cefd1113d39
Rachel Greaner <rgreaner8@gmail.com>
2018-05-14 14:56:45 -0500

    House CSS work

--- client/src/pages/OutdoorPage/OutdoorPage.js
+++ client/src/pages/OutdoorPage/OutdoorPage.js
@@ -14,12 +14,8 @@ class OutdoorPage extends Component {
     render() {
         return (
             <div id="outdoorPage" clasNames="content">
-            <div className="row">
-                <div className="col m">
-                <a href="/indoorplants"><House
-                /></a>
-                </div>
-                <OutsidePlants />
+            {/* <div className="row"> */}
+            <OutsidePlants />
                 <Draggable>
                     <div>
                         <AddPlantIcon
@@ -27,7 +23,19 @@ class OutdoorPage extends Component {
                         /></div>
                 </Draggable>
                 <Plants />
+            {/* </div> */}
+            <div className="row">
             </div>
+            <div className="row">
+            <div className="col l2">
+            </div>
+                <div className="col l2">
+                <a href="/indoorplants"><House
+                /></a>
+                </div>
+                </div>
+               
+            </div>    
         )
     }
 }
