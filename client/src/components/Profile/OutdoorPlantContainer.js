1459e3eba1310acd634ce11f588ead3400638d56
Edwin Hsia <edwinhsia@gmail.com>
2018-05-15 13:55:23 -0500

    updated styling

--- client/src/components/Profile/OutdoorPlantContainer.js
+++ client/src/components/Profile/OutdoorPlantContainer.js
@@ -10,32 +10,25 @@ class OutdoorPlantContainer extends Component {
             }
         }
         return (
-            <div>
-                <h4 className="center-align">Your Outdoor Plants</h4>
-                <ul className="center-align">
-                    {outdoorPlantList.length ? (
-                         outdoorPlantList 
-                    ) : (
-                            <h4>None</h4>
-                        )}
-                </ul>
+            <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <span class="card-title center-align"><h4>Your Outdoor Plants</h4></span>
+                </div>
+                <div class="card-content">
+                        <ul className="center-align">
+                            {outdoorPlantList.length ? (
+                                outdoorPlantList
+                            ) : (
+                                    <ul>None</ul>
+                                )}
+                        </ul>
+                </div>
             </div>
+        </div>
         )
     }
 }
 
 export default OutdoorPlantContainer;
 
-// {this.state.savedArticles.length ? (
-//     <List>
-//       {this.state.savedArticles.map(savedArticles => (
-//         <ListItem key={savedArticles._id}>
-//           <h3>{savedArticles.title}</h3>
-//           <h4>{savedArticles.date}</h4>
-//           <DeleteBtn onClick={() => this.deleteArticle(savedArticles._id)} />
-//         </ListItem>
-//       ))}
-//     </List>
-//   ) : (
-//       <h3>No Results to Display</h3>
-//     )}
--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -21,34 +21,37 @@ class ProfilePage extends Component {
         API.getUser(this.state.userId)
             .then(res => {
                 console.log("get plants", res.data.UserPlants)
-                this.setState({ plants: res.data.UserPlants, badges: res.data.UserBadges, userId:2 })
+                this.setState({ plants: res.data.UserPlants, badges: res.data.UserBadges, userId: 2 })
             })
             .catch(err => console.log(err))
     };
 
     render() {
         return (
-            <div id="profilepage" className="content">
+            <div id="profilepage" className="content center-align">
                 <div id="welcomebanner">
                     <h3> Welcome, Gardener! </h3>
                 </div>
-                <div id="mainprofile" className="row">
-                    <div className="col m3">
+                <div className="row">
+                    <div className="col m6">
                         <ProfilePic />
                     </div>
-                    <div className="col m3">
+                    <div id="badgecontainer">
+                    <div className="col m6">
+                        <BadgeContainer badgesArray={this.state.badges} />
                     </div>
-                    <div className="col m3">
-                        <BadgeContainer badgesArray={this.state.badges}/>
                     </div>
                 </div>
-                <div id="plantcontainers" className="row">
-                    <div className="col m6">
-                        <IndoorPlantContainer plantsArray={this.state.plants}/>
+                <div className="row">
+                    <div id="indoorcontainer">
+                        <div className="col m6">
+                            <IndoorPlantContainer plantsArray={this.state.plants} />
+                        </div>
                     </div>
+                    <div id="outdoorcontainer">
                     <div className="col m6">
-                        <OutdoorPlantContainer plantsArray={this.state.plants}/>
-
+                        <OutdoorPlantContainer plantsArray={this.state.plants} />
+                    </div>
                     </div>
                 </div>
             </div>
