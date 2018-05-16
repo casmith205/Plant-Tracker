b08e272c8fd05f4e8fd6ca02619603ca50920fad
Carolyn Smith <casmith205@gmail.com>
2018-05-16 14:54:31 -0500

    badges Logic works!

--- routes/alerts/badgesLogic.js
+++ routes/alerts/badgesLogic.js
@@ -18,6 +18,9 @@ findUsers = () => {
                 // console.log("THIS IS THE USER WE ARE ON:", userID)
                 let numPlants = res[i].dataValues.UserPlants.length
                 let numBadges = res[i].dataValues.UserBadges.length
+                let deadPlantCount = 0;
+                let alivePlantCount = 0;
+
                 // console.log(numBadges);
                 for (v = 0; v < numBadges; v++) {
                     // console.log("USER HAS THIS BADGE:",res[i].dataValues.UserBadges[v].dataValues.BadgeId);
@@ -28,8 +31,6 @@ findUsers = () => {
                 for (j = 0; j < numPlants; j++) {
                     // GETTING RELEVANT INFO FROM RES
                     let plantStatus = res[i].dataValues.UserPlants[j].dataValues.status;
-                    let deadPlantCount = 0;
-                    let alivePlantCount = 0;
 
                     // console.log("USER PLANT STATUS:", plantStatus);
                     switch (plantStatus) {
@@ -42,46 +43,35 @@ findUsers = () => {
                         default:
                             console.log("needs attn");
                     }
-
-                    // console.log(alivePlantCount);
-                    // console.log(deadPlantCount);
-
-
-                    db.Badge.findAll()
-                        .then(res => {
-                            // console.log("this found all BADGES possible!!", res);
-                            for (q = 0; q < res.length; q++) {
-                                // console.log("-------------------------------------------------------")
-                                let badgeID = res[q].dataValues.id;
-                                // console.log(badgeID);
-                                let numberCriteria = res[q].dataValues.numberCriteria;
-                                let statusCriteria = res[q].dataValues.statusCriteria;
-                                // console.log(numberCriteria,statusCriteria)
-
-                                // IF THE NUMBER & STATUS CRITERIA MATCH THE CURRENT PLANT, 
-                                // PUSH THIS BADGE ID TO AN ARRAY OF BADGES TO ADD
-                                if (numberCriteria <= alivePlantCount || numberCriteria <= deadPlantCount) {
-                                    if (statusCriteria == plantStatus) {
-
-                                        console.log("CURRENT USER", userID)
-                                        console.log("CURRENT BADGE", badgeID);
-                                        console.log("THE PLANT IS ", plantStatus, " AND THIS BADGE REQUIRES ", statusCriteria)
-                                        console.log("THE PLANT IS ", alivePlantCount, deadPlantCount, " AND THIS BADGE REQUIRES ", numberCriteria)
-                                        console.log("DID IT FIND THE CURRENT BADGE IN THE EXISTING??", existingBadgesforUser.indexOf(badgeID));
-                                        if(existingBadgesforUser.indexOf(badgeID) != -1){
-                                            db.UserBadge.create({
-                                                BadgeId: badgeID,
-                                                UserId: userID
-                                              })
-                                              .then(res => console.log("ADDED BADGE ", badgeID, " to USER ", userID))
-                                              .catch(err => res.status(422).json(err));
-                                        }
-                                    }
-
-                                }
-                            }
-                        })
                 }
+
+                db.Badge.findAll()
+                    .then(res => {
+                        // console.log("this found all BADGES possible!!", res);
+                        for (q = 0; q < res.length; q++) {
+                            // console.log("-------------------------------------------------------")
+                            let badgeID = res[q].dataValues.id;
+                            // console.log(badgeID);
+                            let numberCriteria = res[q].dataValues.numberCriteria;
+                            let statusCriteria = res[q].dataValues.statusCriteria;
+                            // console.log(numberCriteria,statusCriteria)
+
+                            // IF THE NUMBER & STATUS CRITERIA MATCH THE CURRENT PLANT, 
+                            // PUSH THIS BADGE ID TO AN ARRAY OF BADGES TO ADD
+
+                            console.log("THIS USER ",userID, "HAS ",alivePlantCount, " ALIVE PLANTS AND ",deadPlantCount, "DEAD PLANTS. THIS BADGE, ", badgeID, ", REQUIRES ",numberCriteria, " ",statusCriteria, " PLANTS!")
+                            if ((numberCriteria <= alivePlantCount && statusCriteria == "alive") ||(numberCriteria <= deadPlantCount && statusCriteria == "dead") ) {
+                                if (existingBadgesforUser.indexOf(badgeID) == -1) {
+                                    db.UserBadge.create({
+                                        BadgeId: badgeID,
+                                        UserId: userID
+                                    })
+                                        .then(res => console.log("ADDED BADGE ", badgeID, " to USER ", userID))
+                                        .catch(err => res.status(422).json(err));
+                                }
+                            };
+                        }
+                    })
             }
         });
 };
