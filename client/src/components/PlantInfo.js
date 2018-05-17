44a835fc2cd84e7a400f1b685a236e06c6eb8825
Carolyn Smith <casmith205@gmail.com>
2018-05-17 11:52:08 -0500

    added minor styling to drop downs

--- client/src/components/PlantInfo.js
+++ client/src/components/PlantInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-
+import "./Dropdown.css";
 
 export const PlantInfo = props => (
     <div>
@@ -8,24 +8,25 @@ export const PlantInfo = props => (
         {/* <!-- Dropdown Structure --> */}
         <ul id={"plantDropdown" + props.plantId}
             className='dropdown-content' >
-            <li>
-                <span onClick={() => props.killPlant(props.plantId + "/dead")}
-                    className="waves-effect waves-light btn-small grey darken-3"> DEAD</span>
-            </li>
-            <li>Plant Name: {props.plantName}  </li>
-            <li> Indoor/outdoor: {props.type} </li>
+           
+            <li> <b>Plant:</b> {props.plantName} </li>
+            <li> <b>Indoor/Outdoor:</b> {props.type} </li>
             {/* <li className="divider" tabIndex="-1"></li> */}
-            <li><i className="material-icons">nature</i>Plant Status: {props.status}</li>
+            <li><i className="material-icons">nature</i> <b>Plant Status:</b> {props.status}</li>
             {!props.needsWater ? (
                 <li>
-                    <i className="material-icons">wb_sunny</i>Water Status: I am full of hydration!
+                    <i className="material-icons">wb_sunny</i> <b>Water Status:</b> I am full of hydration!
              </li>)
                 : (
-                    <li>I'm parched feed me </li>
+                    <li> I'm parched feed me </li>
                 )}
             <li>
                 <a onClick={() => props.waterPlant(props.plantId)}
                     className="btn-small">WATER ME</a>
+            </li>
+            <li>
+                <span onClick={() => props.killPlant(props.plantId + "/dead")}
+                    className="waves-effect waves-light btn-small grey darken-3"> I DIED :( </span>
             </li>
         </ul>
     </div>
