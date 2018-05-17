ba4346fe92007c33ae4295421dc93b53f54a69aa
Edwin Hsia <edwinhsia@gmail.com>
2018-05-17 12:45:53 -0500

    Updated navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,29 +5,33 @@ import logoImage from "../images/logo.png";
 class NavBar extends Component {
 
     handleLogOut = props => {
-
         sessionStorage.clear()
         //setItem("userID", "")
-
     }
 
+    componentDidUpdate() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = window.M.Dropdown.init(elems);
+    }
 
     render() {
         return (
-
-            <div className="navbar-fixed">
-                <nav className='extended'>
-                    <div className="nav-wrapper">
-                        <img id="logoimage" src={logoImage}   alt = "logo"/>
-                        <a href="/" className="brand-logo left" id="logo">Plant Tracker</a>
-                        <ul className="side-nav trigger right" id="mobile-demo">
-                            <li><a href='/indoorplants'><i id="navbar-button" className="small material-icons">home</i>Indoor</a></li>
-                            <li><a href='/outdoorplants'><i id="navbar-button" className="small material-icons">local_florist</i>Outdoor</a></li>
-                            <li><a href='/graveyard'><i id="navbar-button" className="small material-icons">broken_image</i>Graveyard</a></li>
-                            <li><a href='/profile'><i id="navbar-button" className="small material-icons">person</i>Profile</a></li>
-                            <li><a href='/' onClick={this.handleLogOut}><i id="navbar-button" className="small material-icons">brightness_3</i>Logout</a></li>
-                        </ul>
-                    </div>
+            <div>
+                <nav>
+                    <div className="navbar-fixed">
+                        <div className="nav-wrapper">
+                            <img id="logoimage" src={logoImage} alt="logo" />
+                            <a href="/" className="brand-logo" id="logo">Plant Tracker</a>
+                            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><a href='/indoorplants'><i id="navbar-button" className="small material-icons">home</i>Indoor</a></li>
+                                <li><a href='/outdoorplants'><i id="navbar-button" className="small material-icons">local_florist</i>Outdoor</a></li>
+                                <li><a href='/graveyard'><i id="navbar-button" className="small material-icons">broken_image</i>Graveyard</a></li>
+                                <li><a href='/profile'><i id="navbar-button" className="small material-icons">person</i>Profile</a></li>
+                                <li><a href='/' onClick={this.handleLogOut}><i id="navbar-button" className="small material-icons">brightness_3</i>Logout</a></li>
+                            </ul>
+                        </div>
+                    </div >
                 </nav>
                 <ul className="sidenav" id="mobile-demo">
                     <li><a href='/indoorplants'><i id="navbar-button" className="small material-icons">home</i>Indoor</a></li>
@@ -36,8 +40,7 @@ class NavBar extends Component {
                     <li><a href='/profile'><i id="navbar-button" className="small material-icons">person</i>Profile</a></li>
                     <li><a href='/' onClick={this.handleLogOut}><i id="navbar-button" className="small material-icons">brightness_3</i>Logout</a></li>
                 </ul>
-            </div >
-
+            </div>
         )
     };
 };
