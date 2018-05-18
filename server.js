0787d83be7f83a8dd9ad94fb896163a7702512bb
mguldberg <guldberg@sbcglobal.net>
2018-05-17 20:45:27 -0500

    fixed the REACT app star catch all route to go to build instead of public

--- server.js
+++ server.js
@@ -38,10 +38,8 @@ app.use(apiRoutes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function (req, res) {
-    // res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 
 //Sync Database
 models.sequelize.sync().then(function () {
